70afab1807d04db266147d38300672c745d6cc7a
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2022-06-20 01:13:35 +0400

    Added declared date property.

--- src/models/Dividend.php
+++ src/models/Dividend.php
@@ -9,13 +9,16 @@ class Dividend
 {
     private $dividendNet;
     private $paymentDate;
+    private $declaredDate;
 
     public function __construct(
         MoneyValue $dividendNet,
-        DateTime $paymentDate
+        DateTime $paymentDate,
+        DateTime $declaredDate
     ) {
         $this->dividendNet = $dividendNet;
         $this->paymentDate = $paymentDate;
+        $this->declaredDate = $declaredDate;
     }
 
     public function getDividendNet(): MoneyValue
@@ -26,5 +29,10 @@ class Dividend
     public function getPaymentDate(): DateTime
     {
         return $this->paymentDate;
+    }
+
+    public function getDeclaredDate(): DateTime
+    {
+        return $this->declaredDate;
     }
 }
