5ebecba6bb4c4038958adcf1df3f749360f2f4f3
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2022-06-20 01:12:34 +0400

    Added payment date property.

--- src/models/Dividend.php
+++ src/models/Dividend.php
@@ -2,19 +2,29 @@
 
 namespace rocketfellows\TinkoffInvestV1MethodGetDividends\models;
 
+use DateTime;
 use rocketfellows\TinkoffInvestV1Common\models\MoneyValue;
 
 class Dividend
 {
     private $dividendNet;
+    private $paymentDate;
 
-    public function __construct(MoneyValue $dividendNet)
-    {
+    public function __construct(
+        MoneyValue $dividendNet,
+        DateTime $paymentDate
+    ) {
         $this->dividendNet = $dividendNet;
+        $this->paymentDate = $paymentDate;
     }
 
     public function getDividendNet(): MoneyValue
     {
         return $this->dividendNet;
+    }
+
+    public function getPaymentDate(): DateTime
+    {
+        return $this->paymentDate;
     }
 }
