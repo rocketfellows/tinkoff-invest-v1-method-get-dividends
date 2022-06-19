65ac7422d61ccebac4c7a53af79d70ad853dc9d6
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2022-06-20 01:11:01 +0400

    Added dividend net property.

--- src/models/Dividend.php
+++ src/models/Dividend.php
@@ -2,6 +2,19 @@
 
 namespace rocketfellows\TinkoffInvestV1MethodGetDividends\models;
 
+use rocketfellows\TinkoffInvestV1Common\models\MoneyValue;
+
 class Dividend
 {
+    private $dividendNet;
+
+    public function __construct(MoneyValue $dividendNet)
+    {
+        $this->dividendNet = $dividendNet;
+    }
+
+    public function getDividendNet(): MoneyValue
+    {
+        return $this->dividendNet;
+    }
 }
