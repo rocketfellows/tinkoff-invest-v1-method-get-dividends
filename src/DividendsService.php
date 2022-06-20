02a37a3fca9564dfc8335cbd902d50e9851321fe
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2022-06-21 01:48:06 +0400

    Started implementing get all dividends function.

--- src/DividendsService.php
+++ src/DividendsService.php
@@ -2,6 +2,10 @@
 
 namespace rocketfellows\TinkoffInvestV1MethodGetDividends;
 
+use rocketfellows\TinkoffInvestV1MethodGetDividends\exceptions\IncorrectInputsException;
+use rocketfellows\TinkoffInvestV1MethodGetDividends\exceptions\SourceFaultException;
+use rocketfellows\TinkoffInvestV1MethodGetDividends\models\Dividends;
+
 class DividendsService
 {
     private $dividendsRequestInterface;
@@ -9,5 +13,14 @@ class DividendsService
     public function __construct(DividendsRequestInterface $dividendsRequestInterface)
     {
         $this->dividendsRequestInterface = $dividendsRequestInterface;
+    }
+
+    /**
+     * @throws SourceFaultException
+     * @throws IncorrectInputsException
+     */
+    public function getAll(string $figi): Dividends
+    {
+        return $this->dividendsRequestInterface->requestAll($figi);
     }
 }
